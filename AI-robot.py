1fb630f94b6ed21fabda508cc354da04882fa377
Ethan Li <eli31@myyahoo.com>
2025-05-07 08:56:07 -0400

    Use Ollama instead of OpenAI
    ollama.com/download
    pip install ollama

--- AI-robot.py
+++ AI-robot.py
@@ -1,30 +1,38 @@
-import os
-import math
-import random
-import time
-from gpiozero import *
-from openai import OpenAI
-
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-
-import base64
-from openai import OpenAI
-
-client = OpenAI()
-
-prompt = "What is in this image?"
-with open("path/to/image.png", "rb") as image_file:
-    b64_image = base64.b64encode(image_file.read()).decode("utf-8")
-
-response = client.responses.create(
-    model="gpt-4o-mini",
-    input=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "input_text", "text": prompt},
-                {"type": "input_image", "image_url": f"data:image/png;base64,{b64_image}"},
-            ],
-        }
-    ],
-)
+import sys
+if sys.version_info.major < 3:
+    print("This script requires Python 3 or higher. Please upgrade your Python version.")
+    exit()
+else:
+    py_version = str(sys.version)
+import subprocess
+try:
+    result = subprocess.run(["ollama", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    if result.returncode == 0:
+        ollama = True
+        ollama_version = str(result.stdout.strip())
+    else:
+        ollama = False
+except FileNotFoundError:
+   ollama = False
+if ollama == False:
+    print('Ollama is not installed! Please go to https://ollama.com/download to download Ollama for your device.')
+    print('The Script will now exit')
+    exit()
+try:
+    from ollama import chat
+except ImportError:
+    print('Ollama Python Pipeline is not installed! Please install it by running pip install ollama')
+    exit()
+chatmodel = str(input('Model: '))
+prompt = 'user'
+while True:
+    input = str(input('User: '))
+    stream = chat(
+        model=chatmodel,
+        messages=[{'role': prompt 'content': input}],
+        stream=True,
+    )
+    print('AI: ', end='', flush=True)
+    for chunk in stream:
+      print(chunk['message']['content'], end='', flush=True)
